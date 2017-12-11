7a92bcc4e44b0edbe64ae384eb386e013cd96a68
Francesca Bueti <fbueti@gmail.com>
2017-12-11 01:18:49 -0500

    added source

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('src/sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch('src/sass/**/*.scss', ['sass']);
+});
